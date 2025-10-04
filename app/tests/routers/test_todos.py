653cdd5b111adfd65dfd5fb52ad42f78bbcb342a
Patricio <native.devil@gmail.com>
2025-10-04 17:58:00 -0300

    ADD: Test directory

---
+++ app/tests/routers/test_todos.py
@@ -0,0 +1,8 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_index():
+    response = client.get("/items/")
+    assert response.status_code == 200
