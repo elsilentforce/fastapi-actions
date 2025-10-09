88c24fe8f6e574b247e134f841781707fe85cdc1
Patricio <native.devil@gmail.com>
2025-10-08 21:30:56 -0300

    ADD: Dummy comment

--- app/tests/routers/test_todos.py
+++ app/tests/routers/test_todos.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+# This is a comment
 def test_index():
     response = client.get("/items/")
     assert response.status_code == 200
